435470e1371f5f8fc5e54d84b0a3cef33dd2c8fd
Yuvraj <135434326+y2jjjj@users.noreply.github.com>
2025-12-01 16:25:33 +0530

    Create WebhookResponse model class

---
+++ src/main/java/com/bfhl/webhookapp/model/WebhookResponse.java
@@ -0,0 +1,13 @@
+package com.bfhl.webhookapp.model;
+
+public class WebhookResponse {
+    private String webhook;
+    private String accessToken;
+    
+    // Getters and Setters
+    public String getWebhook() { return webhook; }
+    public void setWebhook(String webhook) { this.webhook = webhook; }
+    
+    public String getAccessToken() { return accessToken; }
+    public void setAccessToken(String accessToken) { this.accessToken = accessToken; }
+}
